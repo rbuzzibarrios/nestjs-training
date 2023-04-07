a11797b63dac419454e2af39ddb9053b1aaa0487
rbuzzibarrios <rbuzzibarrios@gmail.com>
2023-04-07 16:40:20 -0400

    add .dockerignore file
    add config for TypeOrmModule in app.module.ts
    
    add config for BooksModule in app.module.ts
    
    add Book entity in book.entity.ts
    
    add BooksController in books.controller.ts
    
    add books module in books.module.ts
    
    add BooksService in books.service.ts
    
    initialize CreateBookDto in create-book.dto.ts
    
    add api/v1 prefix in route at main.ts
    
    install @nestjs/mapped-types
    
    initialize update-book.dto.ts with UpdateBookDto class
    
    [skip ci]

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,30 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { BooksModule } from './books/books.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Book } from './books/entities/book.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env.development', '.env'],
     }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT
+        ? (process.env.DATABASE_PORT as unknown as number)
+        : 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Book],
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+
+    BooksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log(`${process.env.API_PREFIX}/v${process.env.API_VERSION}`);
+
+  app.setGlobalPrefix(`${process.env.API_PREFIX}/v${process.env.API_VERSION}`);
+
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
+  // await app.listen(
+  //   process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  //   process.env.APP_URL as string,
+  // );
   // process.env.APP_URL as string,
 }
 
