44927c198631bbfe08c972bbddf15c102335d0d9
rbuzzibarrios <rbuzzibarrios@gmail.com>
2023-04-13 17:53:47 -0400

    allow process js in tsconfig.json

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -11,20 +10,20 @@ describe('AppController (e2e)', () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [
         AppModule,
-        TypeOrmModule.forRoot({
-          type: 'mysql',
-          host: process.env.DATABASE_HOST,
-          port: process.env.DATABASE_PORT
-            ? (process.env.DATABASE_PORT as unknown as number)
-            : 3306,
-          username: process.env.DATABASE_USER,
-          password: process.env.DATABASE_PASSWORD,
-          database: process.env.DATABASE_NAME,
-          // entities: ['./**/*.entity.ts'],
-          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
-          synchronize: true,
-          autoLoadEntities: true,
-        }),
+        // TypeOrmModule.forRoot({
+        //   type: 'mysql',
+        //   host: process.env.DATABASE_HOST,
+        //   port: process.env.DATABASE_PORT
+        //     ? (process.env.DATABASE_PORT as unknown as number)
+        //     : 3306,
+        //   username: process.env.DATABASE_USER,
+        //   password: process.env.DATABASE_PASSWORD,
+        //   database: process.env.DATABASE_NAME,
+        //   // entities: ['./**/*.entity.ts'],
+        //   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        //   synchronize: true,
+        //   autoLoadEntities: true,
+        // }),
       ],
     }).compile();
 
