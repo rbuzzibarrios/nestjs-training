6b99d7a0554d1b0dcc679ed38374efcd49f02f58
rbuzzibarrios <rbuzzibarrios@gmail.com>
2023-05-02 06:07:57 -0400

    add dist in entities directory
    [skip ci]

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,7 +20,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
       autoLoadEntities: true,
     }),
