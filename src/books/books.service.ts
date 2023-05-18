592ac8473745e48315dbcd234e2fb8e19a0678e0
rbuzzibarrios <rbuzzibarrios@gmail.com>
2023-05-18 12:03:38 -0400

    create BodyBookRequestDto class in body-book-request.dto.ts to accept filters and paginate from client in body request
    use BodyBookRequestDto in findAll() method at books.controller.ts
    change to Post method findAll() action in books.controller.ts
    
    refactor findAll method in books.service.ts to allow filter and paginate books list
    
    create paginate-request.ts file with PaginateRequest class to manage paginated data from body request
    
    [skip ci]

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -3,7 +3,8 @@ import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Book } from './entities/book.entity';
-import { Repository } from 'typeorm';
+import { FindManyOptions, Repository } from 'typeorm';
+import { BodyBookRequestDto } from './dto/body-book-request.dto';
 
 @Injectable()
 export class BooksService {
@@ -15,8 +16,37 @@ export class BooksService {
     return this.bookRepository.save(createBookDto);
   }
 
-  findAll(): Promise<Book[]> {
-    return this.bookRepository.find();
+  findAll(params: BodyBookRequestDto): Promise<Book[]> {
+    let options: FindManyOptions = {
+      order: {
+        [params?.paginate?.orderBy?.column || 'id']:
+          params?.paginate?.orderBy?.order || 'ASC',
+      },
+    };
+
+    if (params.filters != undefined) {
+      options = {
+        where: { ...params.filters },
+        ...options,
+      };
+    }
+
+    if (
+      params.paginate?.page != undefined &&
+      params.paginate?.perPage != undefined
+    ) {
+      options = {
+        ...options,
+        skip:
+          params?.paginate?.page === 1
+            ? 0
+            : ((params?.paginate?.page || 1) - 1) *
+              (params?.paginate?.perPage || 10),
+        take: params?.paginate?.perPage,
+      };
+    }
+
+    return this.bookRepository.find(options);
   }
 
   async findOne(id: number): Promise<Book | null> {
