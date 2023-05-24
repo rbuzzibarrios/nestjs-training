29ac0cb8ce4e6d10c9534b1e8152462d36b10cff
rbuzzibarrios <rbuzzibarrios@gmail.com>
2023-05-24 18:03:51 -0400

    add remove everything in docker container at deploy-docker.sh, and add yarn build
    install fastify in dependencies
    
    [skip ci]

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -1,3 +1,6 @@
+docker compose down
+docker image rmi node:18-alpine
+NODE_ENV=development yarn build
 docker compose up -d -V --build
 docker compose exec nestjs-training yarn migrate
 docker compose exec nestjs-training yarn seed:run
