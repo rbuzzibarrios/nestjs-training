b10f205302f36c49fe5c4ce0542e3696bb64ba55
rbuzzibarrios <rbuzzibarrios@gmail.com>
2023-06-14 13:46:02 -0400

    isntall and config cache module. use cache global and redefine at books controller.
    [skip ci]

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from './data-source';
 import { ExistsConstraint } from './exists/exists.decorator';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,10 +16,21 @@ import { ExistsConstraint } from './exists/exists.decorator';
       cache: true,
       envFilePath: ['.env.testing', '.env.development', '.env'],
     }),
+    CacheModule.register({
+      isGlobal: true
+    }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     BooksModule,
   ],
   controllers: [AppController],
-  providers: [AppService, ExistsConstraint],
+  providers: [
+    AppService,
+    ExistsConstraint,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
+  exports: [CacheModule]
 })
 export class AppModule {}
