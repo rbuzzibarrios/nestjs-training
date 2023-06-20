1d7d71bc99ae471aa9210f37d41eece7b72b35b6
Rodolfo <42749139+rbuzzibarrios@users.noreply.github.com>
2023-06-20 18:22:07 -0400

    Install and config Redis cache (#11)
    * config redis cache options at app.module.ts
    
    add redis service at docker-compose.yml
    
    install all packages for redis at package.json
    
    [skip ci]
    
    * add redis service to CI
    
    * add redis env vars at node.js.yml

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,18 +6,26 @@ import { BooksModule } from './books/books.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmAsyncConfig } from './data-source';
 import { ExistsConstraint } from './exists/exists.decorator';
-import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import {
+  CacheInterceptor,
+  CacheModule,
+  CacheStore,
+} from '@nestjs/cache-manager';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import type { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
+    CacheModule.register<RedisClientOptions>({
+      isGlobal: true,
+      store: redisStore as unknown as CacheStore,
+      url: `redis://${process.env?.REDIS_HOST}:${process.env.REDIS_PORT}`,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
       envFilePath: ['.env.testing', '.env.development', '.env'],
-    }),
-    CacheModule.register({
-      isGlobal: true,
     }),
     TypeOrmModule.forRootAsync(typeOrmAsyncConfig),
     BooksModule,
